9ad895dd750c7862ea60e1626544625c592ef735
Aimee <aimeekisanya@gmail.com>
2025-02-26 17:03:36 +0300

    data types, strings

--- hello.py
+++ hello.py
@@ -1,1 +1,38 @@
-print("hello world time afternoon")
+import random
+# print("hello world time afternoon")
+
+# if 5 > 2:
+    # print("is greater")
+
+# x = 12
+# a = float(x)
+# T = type(a)
+# print(a)
+
+# random_number = random.randrange(0,6)
+# print(random_number)
+# print(random.__all__)
+
+# a = """Lorem ipsum dolor sit amet consectetur adipiscing elit
+# sed do eiusmod tempor incididunt
+# ut labore et dolore magna aliqua."""
+# print(a)
+
+
+# stringArray = "Good morning Mr Apple"
+# print(stringArray.split(' '))
+# print(stringArray[::-1])
+# length = len(stringArray)
+# check = "good" in stringArray.lower()
+# print(length, check)
+
+# for x in "banana":
+#   print(x)
+
+
+# age = 36.456756
+# txt = f"My name is John, I am {age:.2f} " 
+# print(txt)
+
+sentence = "We are the so-called 'Vikings' from the north."
+print(sentence)
